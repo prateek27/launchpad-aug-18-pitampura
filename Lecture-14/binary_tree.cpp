eea8dc16fa9d066b61320ebf732363ad0c50cf52
Prateek Narang <prateeknarang111@gmail.com>
2018-10-13 14:39:17 +0530

    added code

--- Lecture-14/binary_tree.cpp
+++ Lecture-14/binary_tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 //------------Node Class
 class node{
@@ -61,10 +62,112 @@ void printPost(node *root){
 		cout<<root->data<<" ";
 }
 
+int count(node*root){
+	if(root==NULL){
+		return 0;
+	}
+	int c1 = count(root->left);
+	int c2 = count(root->right);
+	return 1 + c1 + c2;
+}
+
+int height(node*root){
+	if(root==NULL){
+		return 0;
+	}
+	int h1 = height(root->left);
+	int h2 = height(root->right);
+	return max(h1,h2) + 1;
+}
+
+void printAtLevelK(node*root,int k){
+	//Base Case
+	if(root==NULL){
+		return;
+	}
+	if(k==0){
+		cout<<root->data<<" ";
+	}
+
+	//Rec Case
+	printAtLevelK(root->left,k-1);
+	printAtLevelK(root->right,k-1);
+}
+
+//Tree Sum
+int sum(node*root){
+
+	if(root==NULL){
+		return 0;
+	}
+	return root->data + sum(root->left) + sum(root->right);
+
+}
+
+//Replace Sum
+int replaceChildSum(node*root){
+	if(root==NULL){
+		return 0;
+	}
+	if(root->left==NULL && root->right==NULL){
+		return root->data;
+	}
+
+	int leftSum = replaceChildSum(root->left);
+	int rightSum = replaceChildSum(root->right);
+
+	int temp = root->data;
+	root->data = leftSum  + rightSum;
+	//	*(root).data = 
+	return temp+root->data;
+}
+
+void printLevelOrder(node*root){
+
+	queue<node*> q;
+	q.push(root);
+	q.push(NULL);
+
+	while(!q.empty()){
+		node* f = q.front();
+
+		if(f==NULL){
+			cout<<endl;
+			q.pop();
+			if(!q.empty()){
+				q.push(NULL);
+			}
+		}
+
+		else{
+			cout<<f->data<<" ";
+			q.pop();
+			if(f->left){
+				q.push(f->left);
+			}
+			if(f->right){
+				q.push(f->right);
+			}
+		}
+
+	}
+
+}
+
+
 
 int main(){
-	node*root = buildPre();
+	node*root = buildRec();
 	printPre(root);
+	cout<<endl;
+	cout<<count(root)<<endl;
+	cout<<height(root)<<endl;
+	//printAtLevelK(root,2);
+
+	//replaceChildSum(root);
+	//printPre(root);
+	printLevelOrder(root);
+
 
 	return 0;
 }
