e711e4e82b22bdec0b73d706e5a506872f6d82dc
aryangulati <aryangulati2810@gmail.com>
2019-10-10 14:41:40 +0530

    added comments

--- Lecture-03/switch_case.cpp
+++ Lecture-03/switch_case.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 int main(){
 
-	char ch;
-	cin>>ch;
+	char ch; 
+	cin>>ch; // taking input of character manually
 
 	switch(ch){
 
-		case 'm' :
-		case 'M' : cout<<"Monday";
-					break;
+		case 'm' :                 //if this comes then  output of M get printed as there is no break
 
-		case 's' :
-		case 'S' : cout<<"Saturday";
+		case 'M' : cout<<"Monday";   //if M comes then Monday print 
+		break;       // due to break switch loops ends
+
+		case 's' :     // if s comes then S is printed as this case has no break statement
+		case 'S' : cout<<"Saturday"; 
 					cout<<"Coding Class";
 					break;
 
-		default : cout<<"Something else";
+		default : cout<<"Something else"; /* if other (val of ch)option comes then default val get printed*/
 	}
 
 
